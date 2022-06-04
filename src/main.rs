ebb5e17dd2bf2f7b36d1ae680d86381254609456
anthony-khong <anthony.kusumo.khong@gmail.com>
2022-06-04 09:48:59 +0700

    first GitLab API hit example

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv::dotenv().ok();
+    let url = load_env("SOURCE_GITLAB_URL");
+    let token = load_env("SOURCE_GITLAB_TOKEN");
     let response = gitlab_migrator::http::CLIENT
-        .get("https://ifconfig.me")
+        .get(format!("{}/groups/", url))
+        .query(&[("per_page", "100")])
+        .header("PRIVATE-TOKEN", token)
         .send()
         .await?;
     println!("{:#?}", response.text().await?);
     Ok(())
+}
+
+pub fn load_env(key: &str) -> String {
+    let result = std::env::var(key);
+    result.unwrap_or_default()
 }
